d0dc4e527ced2ba2ea92b04e3d1c6b463a3fd498
Maxtan Huynh <mhuynh1002@jh-mba158-15.d214.org>
2018-04-25 14:57:16 -0500

    commit

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -10,13 +10,29 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    var topPaddle = SKSpriteNode()
+    var bottomPaddle = SKSpriteNode()
+    var leftPaddle = SKSpriteNode()
+    var rightPaddle = SKSpriteNode()
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
+        topPaddle = self.childNode(withName: "topPaddle") as! SKSpriteNode
+        bottomPaddle = self.childNode(withName: "bottomPaddle") as! SKSpriteNode
+        leftPaddle = self.childNode(withName: "leftPaddle") as! SKSpriteNode
+        rightPaddle = self.childNode(withName: "rightPaddle") as! SKSpriteNode
+        
+        
+        
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         self.physicsBody = border
     }
     func didBegin(_ contact: SKPhysicsContact) {
         print(contact)
     }
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        topPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        
+    }
 }
